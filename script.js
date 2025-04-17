ddb8dc29481fb19d94cdf491b04da60606a75096
Nifla <fathimanifla1118@gmail.com>
2025-04-17 15:49:08 +0530

    new portfolio update

---
+++ script.js
@@ -0,0 +1,268 @@
+
+   
+   
+   
+   
+       
+
+
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    emailjs.send('service_oxa25bh', 'template_k0ib2ba', {
+        name: name,
+        email: email,
+        message: message
+    })
+    .then(function(_response) {
+        alert('Message sent successfully!');
+        document.getElementById('contactForm').reset();
+    }, function(error) {
+        alert('Error sending message: ' + JSON.stringify(error));
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   const jobTitleElement = document.querySelector('.hero-section h1 strong');
+    const jobTitles = ["Frontend Developer", "Web Developer", "Full Stack Developer"];
+    let currentIndex = 0;
+    
+    function cycleJobTitles() {
+      if (jobTitleElement) {
+        jobTitleElement.textContent = jobTitles[currentIndex];
+        currentIndex = (currentIndex + 1) % jobTitles.length;
+        setTimeout(cycleJobTitles, 500); 
+      }
+    }
+    
+   
+    
+    
+    
+    
+    
+
+    
+    document.addEventListener('DOMContentLoaded', cycleJobTitles);
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
